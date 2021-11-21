0e3395cb2d04280937b1026ce0d4851895845baa
Anmol Singh <anmolsinghmoti@gmail.com>
2021-11-20 21:32:15 -0500

    Login page added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 // import express & router controllers
 const express = require('express');
-const { DisplayHomePage, DisplayAboutPage, DisplayContactPage } = require("../controllers");
+const { DisplayHomePage, DisplayAboutPage, DisplayContactPage,DisplayLoginPage } = require("../controllers");
 // setup router for export
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.get('/', DisplayHomePage);
 router.get('/home', DisplayHomePage);
 router.get('/about', DisplayAboutPage);
 router.get('/contact', DisplayContactPage);
+router.get('/login',  DisplayLoginPage);
 
 // EXPORTS
 module.exports = router;
